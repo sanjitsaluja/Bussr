4845fc66d4c672f3addce04cee3233625ae264a0
Sanjit Saluja <loopwhile1@gmail.com>
2012-01-25 23:01:58 -0800

    Fix map, start working on navigation into stop details

--- bussr/bussr/webapp/static/ctamap.js
+++ bussr/bussr/webapp/static/ctamap.js
@@ -62,32 +62,123 @@ var BusStopFetcher = function (resultHandler) {
     }
 };
 
+MarkerManager = function (googleMapObj, markerDelegate) { return {
+	activeMarkers : [],	// stopId -> annotation
+	activeStopIds : {}, // stopId's current visible stopId - ''
+	
+	//+----------------------------------------------------------------
+	// Add a marker to the map. 
+	// Add a click event to the marker to 
+	// display a bubble.
+	//-----------------------------------------------------------------
+   	addMarker : function(stop) {
+		var stopId = stop.stopId;
+		var mapMarker;
+		var marker;
+		var lat, lng;
+		var position;
+		
+		if (this.activeMarkers[stopId] === undefined) {
+			lat = parseFloat(stop["lat"]);
+			lng = parseFloat(stop["lng"]);
+			position = new google.maps.LatLng(lat, lng);
+			
+			marker = new google.maps.Marker({
+				position: position,
+				map: googleMapObj,
+				draggable: false,
+				clickable: true
+			});
+
+			// add a marker to the map. also add it to our ivar set
+			mapMarker = this.activeMarkers[stopId] = {
+				'stopId': stopId,
+				'lat': lat,
+				'lng': lng,
+				'stop': stop,
+				'marker': marker
+			};
+			
+			// display the bubble on click
+			google.maps.event.addListener(marker, 'click', function(){
+				markerDelegate.displayBubble(mapMarker);
+			});
+		}
+    },
+    
+    //+----------------------------------------------------------------
+	// Delete a marker from the map and our local set
+	//-----------------------------------------------------------------
+	deleteMarker: function(stopId){
+		if (this.activeMarkers[stopId]) {
+			if(this.activeMarkers[stopId].marker) {
+	    		this.activeMarkers[stopId].marker.setMap(null);
+	    	}
+			
+	        this.activeMarkers[stopId] = null;
+	        delete this.activeMarkers[stopId];
+		}
+	},
+    
+    updateMarkersWithStops : function(newStops) {
+    	// start with deleting all stops
+    	var stopIdsToDelete = this.clone(this.activeStopIds);
+    	this.activeStopIds = {}
+    	
+    	for (var i in newStops) {
+    		stop = newStops[i];
+    		stopId = stop.stopId;
+    		this.activeStopIds[stopId] = '';
+    		if (stopIdsToDelete[stopId] !== undefined) {
+	        	delete stopIdsToDelete[stopId];
+    		}
+    	}
+    	
+    	for (stopId in stopIdsToDelete) {
+    		this.deleteMarker(stopId);
+    	}
+    	
+		for (var j = 0; j < newStops.length; j++) {
+			stop = newStops[j];
+			this.addMarker(stop);
+		}
+    },
+	
+	clone : function(obj) {
+		return jQuery.extend(true, {}, obj);
+	},
+}
+};
+
 
 var earthQuakeMap = {
 	map: null,							// google map object
-	activeMarkers: [],					// hold active markers
 	busStopFetcher: null,				// ajax fetcher
 	searchDelay: null,					// delay the search, prevent overzealous searching
 	viewport : {x: 0, y: 0},			// holds viewport dimensions
+	markerManager : null,
 	
     // resizes map div and triggers the Google Map API's resize event 
     resizeMap : function (mapDivId) {
-    	$("#" + mapDivId).css('height', 300);
+    	//$("#" + mapDivId).css('height', 300);
     	
-        /*var that = this;
+        var that = this;
         $(document).ready(function () {
     		console.log('resizeMap');
     		var oldVP = jQuery.extend(true, {}, that.viewport);
     		var h = $(window).height();
     	    var w = $(window).width();
     	    var top = $("#"+mapDivId).position().top;
-    	    $("#"+mapDivId).css('height',h-top);
+    	    var footerHeight = $('.page_footer').height();
+    	    var headerHeight = $('.page_header').height()
+    	    console.log(h, top, footerHeight, headerHeight);
+    	    $("#"+mapDivId).css('height',h-footerHeight-headerHeight-4);
     	    google.maps.event.trigger(that.mapObj, 'resize');
-    	});*/
+    	});
     },
 	
 	stopFetcherGotStops: function(stops) {
-    	this.plotStops(stops);
+    	this.markerManager.updateMarkersWithStops(stops);
 	},
 	
 	searchAroundMapCenter: function() {
@@ -131,89 +222,13 @@ var earthQuakeMap = {
 		// Create the bus stop fetcher with ourselves as the delegate
 		this.busStopFetcher = BusStopFetcher(earthQuakeMap);
 		this.initializeGoogleMapHelper();
-	},
-	
-	plotStops : function(stops) {
-		this.deleteAllMarkers();
-		for (var j = 0; j < stops.length; j++) {
-			stop = stops[j];
-			console.log(stop);
-			this.addMarker(stop);
-		}
+		this.markerManager = MarkerManager(this.map, earthQuakeMap);
 	},
 
-	//+----------------------------------------------------------------
-	// Delete all marker from the map and our local set
-	//-----------------------------------------------------------------		
-	deleteAllMarkers : function() {
-		if (this.activeMarkers) {
-			for (var i in this.activeMarkers) {
-				if (this.activeMarkers[i]) {
-					this.deleteMarker(i);
-				}
-			}
-		}
-	},
-	
 	displayBubble: function(marker) {
-		console.log(marker)
+		$.mobile.changePage('/stop/'+marker.stop.stopId+'/');
 	},
-	//+----------------------------------------------------------------
-	// Delete a marker from the map and our local set
-	//-----------------------------------------------------------------
-	deleteMarker: function(stopId){
-		console.log(this.activeMarkers.length);
-		if (this.activeMarkers[stopId]) {
-			if(this.activeMarkers[stopId].marker) {
-	    		this.activeMarkers[stopId].marker.setMap(null);
-	    	}
-			
-	        this.activeMarkers[stopId] = null;
-	        delete this.activeMarkers[stopId];
-		}
-		console.log(this.activeMarkers.length);
-	},
-	
-	//+----------------------------------------------------------------
-	// Add a marker to the map. Add a click event to the marker to 
-	// display a bubble.
-	//-----------------------------------------------------------------
-    addMarker : function(stop) {
-		var stopId = stop.stopId;
-		var mapMarker;
-		var marker;
-		var lat, lng;
-		var position;
-		var that = this;
-		
-		if (this.activeMarkers[stopId] === undefined) {
-			lat = parseFloat(stop["lat"]);
-			lng = parseFloat(stop["lng"]);
-			position = new google.maps.LatLng(lat, lng);
-			
-			marker = new google.maps.Marker({
-				position: position,
-				map: this.map,
-				draggable: false,
-				clickable: true
-			});
 
-			// add a marker to the map. also add it to our ivar set
-			mapMarker = this.activeMarkers[stopId] = {
-				'stopId': stopId,
-				'lat': lat,
-				'lng': lng,
-				'stop': stop,
-				'marker': marker
-			};
-			
-			// display the bubble on click
-			google.maps.event.addListener(marker, 'click', function(){
-				that.displayBubble(mapMarker);
-			});
-		}
-    },
-	
 	// Private methods
 	// Abort the pending ajax fetch
 	abortPendingFetch : function() {
