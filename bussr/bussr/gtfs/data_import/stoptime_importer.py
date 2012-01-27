6433e31fed21af397bef658fe69512576372dd16
Sanjit Saluja <loopwhile1@gmail.com>
2012-01-26 23:02:34 -0800

    Stop time importer

---
+++ bussr/bussr/gtfs/data_import/stoptime_importer.py
@@ -0,0 +1,50 @@
+'''
+Created on Jan 21, 2012
+@author: sanjits
+'''
+import csv
+from bussr.gtfs.models import StopTime, Trip, Stop
+import re
+
+class StopTimeImporter(object):
+    '''
+    Import stops.txt gtfs file
+    '''
+
+    def __init__(self, filename):
+        '''
+        Constructor
+        '''
+        self.filename = filename
+        
+    def parse(self):
+        '''
+        Parse the stoptimes.txt gtfs file
+        '''
+        reader = csv.DictReader(open(self.filename, 'r'))
+        for row in reader:
+            stopTime = StopTime()
+            stopTime.trip = self.tripForId(row['trip_id'])
+            stopTime.stop = self.stopForId(row['stop_id'])
+            stopTime.stopSequence = row['stop_sequence']
+            stopTime.arrivalSeconds = self.parseTime(row['arrival_time'])
+            stopTime.departureSeconds = self.parseTime(row['departure_time'])
+            stopTime.headSign = 'stop_headsign' in row and row['stop_headsign'] or None
+            stopTime.pickUpType = 'pickup_type' in row and row['pickup_type'] or None
+            stopTime.dropOffType = 'dropoff_type' in row and row['dropoff_type'] or None
+            stopTime.distanceTraveled = 'shape_dist_traveled' in row and float(row['shape_dist_traveled']) or None
+            stopTime.save()
+        
+    def tripForId(self, tripId):
+        return Trip.objects.get(tripId=tripId)
+    
+    def stopForId(self, stopId):
+        return Stop.objects.get(stopId=stopId)
+        
+    def parseTime(self, value):
+        parts = re.split(':', value)
+        hr = int(parts[0])
+        mn = int(parts[1])
+        ss = int(parts[2])
+        return ss + (mn * 60) + (hr * 3600)
+    
